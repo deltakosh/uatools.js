f23c968d4b59433493ea5987dbb0a354576dc6a8
David Catuhe <david.catuhe@live.fr>
2014-10-06 09:35:01 -0700

    First alpha release

---
+++ uatools.js
@@ -0,0 +1,33 @@
+﻿var UATOOLS;
+
+(function (UATOOLS) {
+    var currentUA = navigator.userAgent || "";
+
+    var currentLowerUA = currentUA.toLowerCase();
+
+    // Features
+    UATOOLS.IsMobile = function() {
+        return currentUA.indexOf("mobile") >= 0;
+    }
+
+    UATOOLS.GetUserAgentString = function () {
+        return currentUA;
+    }
+
+    UATOOLS.GetOperatingSystem = function () {
+        // Windows Phone
+        if (currentLowerUA.indexOf("Windows Phone") >= 0) {
+            return "Windows";
+        }
+
+        // Windows
+        if (currentLowerUA.indexOf("windows") >= 0) {
+            return "Windows";
+        }
+
+        // Android
+        if (currentLowerUA.indexOf("android") >= 0) {
+            return "Android";
+        }
+    }
+})(UATOOLS || (UATOOLS = {}));
