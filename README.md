0d824fd1f087378a83430d6422fee2672552cd79
David Catuhe <david.catuhe@live.fr>
2014-10-06 10:26:06 -0700

    More OS supported

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ uatools.js
 
 A set of tools to help you analyse user agent strings.
 
+You can try it live just [here](www.catuhe.com/uatools)
+
+Feel free to contribute to get even more accurate results!
+
--- uatools.js
+++ uatools.js
@@ -7,17 +7,17 @@
 
     // Features
     UATOOLS.IsMobile = function() {
-        return currentUA.indexOf("mobile") >= 0;
+        return currentLowerUA.indexOf("mobile") >= 0;
     }
 
     UATOOLS.GetUserAgentString = function () {
         return currentUA;
     }
 
-    UATOOLS.GetOperatingSystem = function () {
+    UATOOLS.GetOperatingSystem = function() {
         // Windows Phone
-        if (currentLowerUA.indexOf("Windows Phone") >= 0) {
-            return "Windows";
+        if (currentLowerUA.indexOf("windows phone") >= 0) {
+            return "Windows Phone";
         }
 
         // Windows
@@ -28,6 +28,36 @@
         // Android
         if (currentLowerUA.indexOf("android") >= 0) {
             return "Android";
+        }
+
+        // iOS
+        if (currentLowerUA.indexOf("apple-i") >= 0) {
+            return "iOS";
+        }
+
+        // BlackBerry
+        if (currentLowerUA.indexOf("blackberry") >= 0) {
+            return "BlackBerry";
+        }
+
+        // BlackBerry
+        if (currentLowerUA.indexOf("kindle") >= 0) {
+            return "Kindle";
+        }
+
+        // Macintosh
+        if (currentLowerUA.indexOf("macintosh") >= 0) {
+            return "Macintosh";
+        }
+
+        // Linux
+        if (currentLowerUA.indexOf("linux") >= 0) {
+            return "Linux";
+        }
+
+        // OpenBSD
+        if (currentLowerUA.indexOf("openbsd") >= 0) {
+            return "OpenBSD";
         }
     }
 })(UATOOLS || (UATOOLS = {}));
