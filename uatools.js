763b746e0d8c960713adb3490c2d86d1dad82a50
Etienne Margraff <etmargra@microsoft.com>
2016-10-19 16:43:15 -0700

    Adding button for store app download

--- uatools.js
+++ uatools.js
@@ -164,4 +164,67 @@
         return false;
     }
 
+    // Experimental
+    window.addEventListener("DOMContentLoaded", function() {
+        var storeLinks = document.getElementsByClassName("winstore-link");
+
+        for(var i = 0, len = storeLinks.length; i < len; i++) {
+            var storeLink = storeLinks[i];
+
+            //if windows version below anniversary
+            if(!UATOOLS.IsWindowsAnniversaryUpdateOrAbove()){
+                //hide the link
+                storeLink.style.display = "none";
+                return;
+            }
+
+            var campainid, storeid;
+
+            if(storeLink.attributes["data-winstore-id"]) 
+                storeid = storeLink.attributes["data-winstore-id"].value;
+
+            if(storeid){
+                //Get campain id if there is any
+                if(storeLink.attributes["data-winstore-cid"]) 
+                    campainid = storeLink.attributes["data-winstore-cid"].value;
+
+                //default value for size
+                var size = "medium";
+                if(storeLink.attributes["data-winstore-badge-size"]) 
+                    size = storeLink.attributes["data-winstore-badge-size"].value || size;
+            
+                var img = document.createElement("img");
+                img.src = "https://assets.windowsphone.com/f2f77ec7-9ba9-4850-9ebe-77e366d08adc/English_Get_it_Win_10_InvariantCulture_Default.png";
+                img.alt = "Get it on Windows 10";
+                
+                size = size || "medium";
+                switch(size){
+                    case "small":
+                        img.height = 20;
+                        break;
+                    case "medium":
+                        img.height = 40;
+                        break;
+                    case "large":
+                        img.height = 80;
+                        break;
+                }
+
+                storeLink.appendChild(img);
+                storeLink.href = "https://www.microsoft.com/store/apps/" + storeid;
+
+                if(campainid)
+                    storeLink.href += "&cid=" + campainid;
+
+                campainid = undefined;
+                storeid = undefined;
+            }
+            else{
+                //hide the link
+                storeLink.style.display = "none";
+                console.log("Found a Windows Store anchor with no data-winstore-id. Please specify it.");
+            }
+        }
+    }, false);
+
 })(UATOOLS || (UATOOLS = {}));
